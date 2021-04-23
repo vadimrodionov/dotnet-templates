89e8963d968384d16232b68b1416e4a9afb2522a
Vadim Rodionov <rodionov17v@gmail.com>
2021-04-23 18:26:46 +0300

    Scaffolded full solution structure for lessons 1 and 2

---
+++ ITStep.CSharp.Templates/templates/lesson-1/tests/hometasks/Hometask-03-Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StudentName.Hometask_03_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
