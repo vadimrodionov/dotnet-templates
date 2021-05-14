4f4dec2e0077a543c6d85fb95c996d51ca429aca
Vadim Rodionov <rodionov17v@gmail.com>
2021-05-15 00:39:58 +0300

    Added solution template for lesson 3 into ITStep.CSharp.Templates

---
+++ ITStep.CSharp.Templates/templates/lesson-3/src/hometasks/Hometask-02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentName.Hometask_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ITStep.CSharp.Templates/templates/lesson-3/src/hometasks/Hometask-03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentName.Hometask_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
